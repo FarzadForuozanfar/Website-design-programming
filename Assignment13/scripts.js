5bc9ec954898120def7d687ad49f7abb2ffffd0a
farzad_foroozanfar <foruozanfar2000f@gmail.com>
2022-04-09 00:19:42 +0430

    second commit

--- Assignment13/scripts.js
+++ Assignment13/scripts.js
@@ -3,49 +3,154 @@ var buttons = [ [cells[0],cells[1],cells[2]],
                 [cells[3],cells[4],cells[5]],
                 [cells[6],cells[7],cells[8]]];
 
-
 var ply = "X";
 var flags = [[null, null, null],
              [null, null, null],    
              [null, null, null]];
 
-var mode = document.getElementsByName("play-mode")
-//console.log(mode)
-console.log(mode[0].checked,mode[1].checked)
+var choose = true;
+
+var mode1 = document.getElementById("one-player");
+var mode2 = document.getElementById("two-player");
+
+var playing_mode;
+
+var tiles = 0;
+
+var scoresX = document.getElementById("Xscore");
+var scoresO = document.getElementById("Oscore");
+var draws = document.getElementById("draw");
+
+
+let scoresX_cnt = {
+    value: 0
+};
+let scoresO_cnt = {
+    value: 0
+};
+let draw_cnt = {
+    value: 0
+};
+function change_mode(player)
+{
+    if(player == 1)
+    {
+        playing_mode = "one-player";
+        console.log(playing_mode)
+        mode2.disabled = true;
+    }
+    else if(player == 2)
+    {
+        playing_mode = "two-player";
+        console.log(playing_mode)
+        mode1.disabled = true;
+    }
+             
+}
 function playing(x, y)
 {
     if (flags[x][y] == null)
     {
-        if (ply == "X")
+        
+        if(playing_mode == "one-player")
         {
-            flags[x][y] = "X";
-            buttons[x][y].innerHTML = "X";
-            buttons[x][y].classList.add("X");
-            ply = "O";
+                if (ply == "X")
+            {
+                flags[x][y] = "X";
+                buttons[x][y].innerHTML = "X";
+                buttons[x][y].classList.add("X");
+                ply = "O";
+                tiles++;
+                
+                winner = check_play()
+                if (winner == "player X")
+                    Game_over("Winner is Player X", scoresX_cnt, scoresX)
+                else if (tiles == 9)
+                    Game_over("Match Was Draw", draw_cnt, draws)
+                
+                    
+            }
+                bot()
+                choose = true;
+                tiles++;
+                winner = check_play()
+                if (winner == "player O")
+                    Game_over("Winner is CPU", scoresO_cnt, scoresO)
         }
-    
-        else if(ply == "O")
+            
+        else if(playing_mode == "two-player")
         {
-            flags[x][y] = "O";
-            buttons[x][y].innerHTML = "O";
-            buttons[x][y].classList.add("O");
-            ply = "X";
+            if (ply == "X")
+            {
+                flags[x][y] = "X";
+                buttons[x][y].innerHTML = "X";
+                buttons[x][y].classList.add("X");
+                ply = "O";
+                tiles++;
+                winner = check_play()
+                if (winner == "player X")
+                    Game_over("Winner is Player X", scoresX_cnt, scoresX)
+                else if (tiles == 9)
+                    Game_over("Match Was Draw", draw_cnt, draws)
+                
+                
+            }
+            else if (ply == "O")
+            {
+                flags[x][y] = "O";
+                buttons[x][y].innerHTML = "O";
+                buttons[x][y].classList.add("O");
+                ply = "X";
+                tiles++;
+                winner = check_play()
+
+                if (winner == "player O")
+                    Game_over("Winner is Player O", scoresO_cnt, scoresO)
+            }
         }
     }
 
 }
-
-function check_play_mode()
+function bot()
 {
-    /*if (mode[0].checked == true)
+    
+    
+    while(tiles < 9 && choose)
     {
-        console.log("yes berar", mode[1].checked)
+        i = getRndInteger(0,3);
+        j = getRndInteger(0,3);
+        console.log(i, j)
+        if(flags[i][j] == null )
+            {
+                flags[i][j] = "O";
+                buttons[i][j].innerHTML = "O";
+                buttons[i][j].classList.add("O");
+                ply = "X";
+                choose = false;
+                break
+            }
+    
     }
-    
-    else
-        console.log("no berar" , mode[0].checked)*/
-    
 }
+function Game_over(winner, cnt, score)
+{
+    for(var i = 0 ; i < 3 ; i++)
+    {
+        for(var j = 0 ; j < 3 ; j++)
+        {
+            buttons[i][j].disabled = true;
+        }
+    }
+    choose = false;
+    document.getElementById("winner").innerHTML = winner;
+    cnt.value++;
+    score.innerHTML = cnt.value;
+}
+function getRndInteger(min, max) 
+{
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function check_play()
 {
     
@@ -102,4 +207,36 @@ function check_play()
             return "player X";
         if (flags[0][0] == 'O' && flags[1][1] == 'O' && flags[2][2] == 'O' || flags[0][2] == 'O' && flags[1][1] == 'O' && flags[2][0] == 'O')
             return "player O";
+}
+
+function Reset_game()
+{
+    mode1.disabled = false;
+    mode2.disabled = false;
+    mode1.checked = false;
+    mode2.checked = false;
+    document.getElementById("winner").innerHTML = "";
+    tiles = 0;
+    if(ply != "X")
+        ply = "X"
+    playing_mode = null;
+    for(var i = 0 ; i < 3 ; i++)
+    {
+        for(var j = 0 ; j < 3 ; j++)
+        {
+            buttons[i][j].disabled = false;
+        }
+    }
+    for(var i = 0 ; i < 3 ; i++)
+    {
+        for(var j = 0 ; j < 3 ; j++)
+        {
+            buttons[i][j].innerHTML="";
+            flags[i][j]=null;
+            buttons[i][j].classList.remove("X");
+            buttons[i][j].classList.remove("O");
+
+        }
+    }
+
 }
