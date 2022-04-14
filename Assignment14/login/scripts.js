e5510012a38e75ab027f9adf86144cdfed15b134
farzad_foroozanfar <foruozanfar2000f@gmail.com>
2022-04-15 00:01:36 +0430

    first commit

---
+++ Assignment14/login/scripts.js
@@ -0,0 +1,58 @@
+function ChangeMode(){
+    var dark = document.getElementById("dark-check");
+    var back = document.getElementById("form");
+    if (dark.checked)
+        back.style.backgroundColor = "darkgray";
+    else 
+        back.style.backgroundColor = "lightgray";
+}
+
+function addchild(){
+    var childeren = document.getElementById("childeren");
+
+    var new_input = document.createElement("INPUT");
+    new_input.setAttribute("type", "text");
+    new_input.setAttribute("id", "in");
+
+    childeren.appendChild(new_input);
+}
+function removechild(){
+    var childeren = document.getElementById("childeren");
+    if(childeren.childNodes[0])
+        childeren.removeChild(childeren.lastChild);
+}
+function havechild(){
+    var child_btn = document.getElementById("child-title");
+    if (document.getElementById("child").checked)
+    {
+        child_btn.style.display = "block";
+        document.getElementById("childeren").style.display = "block";
+    }
+    else
+    {
+        child_btn.style.display = "none";
+        document.getElementById("childeren").style.display = "none";
+    }
+}
+function single(){
+    var wife = document.getElementsByClassName("wife");
+    wife[0].style.display = "none";
+    wife[1].style.display = "none";
+}
+function married(){
+    var label = document.getElementById("lapel-child");
+    var child = document.getElementById("child");
+    var wife = document.getElementsByClassName("wife");
+    wife[0].style.display = "block";
+    wife[1].style.display = "block";
+    child.style.display = "block";
+    label.style.display = "block";
+}
+function login(){
+    
+    var form = document.getElementById("form");
+    var title = document.getElementById("login-title")
+    title.style.display = "flex";
+    form.style.display = "block";
+    
+}
